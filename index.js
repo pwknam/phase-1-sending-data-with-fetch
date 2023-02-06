b53915c1a9595bda2b2d1974c0532cc460dce792
Kyushik <kyushik95@gmail.com>
2023-02-06 11:39:57 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(username, useremail) {
+    return fetch("http://localhost:3000/users", {
+        method: `POST`,
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: username,
+            email: useremail,
+        })
+    })
+
+    .then(function (response) {
+        return response.json()
+    })
+
+    .then(function (object) {
+        document.body.append(object.id)
+        console.log(object.id)
+    })
+
+    .catch(function(message){
+        document.body.append(message)
+    })
+
+}
+
+submitData("Kyushik", "pwkn95@gmail")
